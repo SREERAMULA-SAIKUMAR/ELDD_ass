cc53b66db0e4de76d8ee3f74e5028f915bb77136
SREERAMULA-SAIKUMAR <sreeramulasaikumar@gmail.com>
2021-12-28 22:02:20 +0530

    character device driver program added

---
+++ chardevice/.chardriver.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/sai/saik2/pi/ELDD/chardevice/chardriver.mod := { echo  /home/sai/saik2/pi/ELDD/chardevice/chardriver.o;  echo; } > /home/sai/saik2/pi/ELDD/chardevice/chardriver.mod
---
+++ chardevice/.modules.order.cmd
@@ -0,0 +1,1 @@
+cmd_/home/sai/saik2/pi/ELDD/chardevice/modules.order := {   echo /home/sai/saik2/pi/ELDD/chardevice/chardriver.ko; :; } | awk '!x[$$0]++' - > /home/sai/saik2/pi/ELDD/chardevice/modules.order
